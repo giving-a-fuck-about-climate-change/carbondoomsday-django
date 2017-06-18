6cd0d8b1d81a3ddb46794a57eb13ad14a89a4526
Luke Murphy <lukewm@riseup.net>
2017-06-19 00:13:35 +0100

    Refactor test and add missing docstring.

--- carbondoomsday/carbondioxide/models.py
+++ carbondoomsday/carbondioxide/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 
 class CO2Measurement(models.Model):
+    """CO2 measurement model."""
     date = models.DateField(unique=True)
     ppm = models.DecimalField(max_digits=5, decimal_places=2)
--- carbondoomsday/carbondioxide/test/test_models.py
+++ carbondoomsday/carbondioxide/test/test_models.py
@@ -1,6 +1,7 @@
-"""Model testing."""
+"""Model tests."""
 
 from datetime import date
+from decimal import Decimal
 
 import pytest
 from django.db.utils import IntegrityError
@@ -11,12 +12,12 @@ pytestmark = pytest.mark.django_db(transaction=True)
 
 
 def test_co2_measurement():
-    today = date.today()
-    ppm = 449.15
-
+    today, ppm = date.today(), Decimal("449.15")
     co2 = CO2Measurement.objects.create(date=today, ppm=ppm)
-    assert co2.date == today
-    assert co2.ppm == ppm
+
+    created = CO2Measurement.objects.get(date=str(co2.date))
+    assert created.date == today
+    assert created.ppm == ppm
 
     with pytest.raises(IntegrityError):
         CO2Measurement.objects.create(date=today, ppm=None)
