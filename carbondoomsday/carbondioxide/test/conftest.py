affc1f10e073b356a0ce3657eaa0e38f45c67f1b
Luke Murphy <lukewm@riseup.net>
2017-06-18 13:36:48 +0100

    Add mocked CSV testing fixture.

--- carbondoomsday/carbondioxide/test/conftest.py
+++ carbondoomsday/carbondioxide/test/conftest.py
@@ -1,3 +1,20 @@
 """Test fixtures."""
 
 import pytest
+
+
+@pytest.fixture
+def mocked_co2_csv(mocker):
+    mocked_csv = (
+        "Date,day,month,week\n"
+        "2017-06-08,409.49,409.65,409.65\n"
+        "2017-06-09,409.69,,409.65\n"
+        "2017-06-10,409.15,,409.65\n"
+        "2017-06-11,,,\n"
+        "2017-06-12,409.75,,\n"
+    )
+
+    mocked = mocker.Mock()
+    mocked.content = bytes(mocked_csv, "utf-8")
+    target = "carbondoomsday.carbondioxide.tasks.requests.get"
+    mocker.patch(target, return_value=mocked)
