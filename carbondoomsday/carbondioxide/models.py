2b21e9fe1ab2366b2049670bb6fe9e6101d61a28
Luke Murphy <lukewm@riseup.net>
2017-06-18 13:36:19 +0100

    Add CO2Measurement model.

--- carbondoomsday/carbondioxide/models.py
+++ carbondoomsday/carbondioxide/models.py
@@ -1,3 +1,8 @@
 """Database models."""
 
 from django.db import models
+
+
+class CO2Measurement(models.Model):
+    date = models.DateField(unique=True)
+    ppm = models.DecimalField(max_digits=5, decimal_places=2)
