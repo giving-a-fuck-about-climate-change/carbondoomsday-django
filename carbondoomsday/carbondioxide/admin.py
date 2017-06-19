5c93bb15f12b7ed1ea68aa7212f38b35274d5d2e
Luke Murphy <lukewm@riseup.net>
2017-06-19 15:43:05 +0100

    Register CO2Measurement model with admin.

--- carbondoomsday/carbondioxide/admin.py
+++ carbondoomsday/carbondioxide/admin.py
@@ -1,3 +1,9 @@
 """Admin models."""
 
-# from django.contrib import admin
+from carbondoomsday.carbondioxide.models import CO2Measurement
+from django.contrib import admin
+
+
+@admin.register(CO2Measurement)
+class CO2MeasurementAdmin(admin.ModelAdmin):
+    readonly_fields = ("date", "ppm",)
