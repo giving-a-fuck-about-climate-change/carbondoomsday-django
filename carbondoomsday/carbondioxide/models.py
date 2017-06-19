6bb4540903e673e05cf592182a58f2480911b6e0
Luke Murphy <lukewm@riseup.net>
2017-06-19 15:43:20 +0100

    Add explicit verbose name for CO2Measurement model.

--- carbondoomsday/carbondioxide/models.py
+++ carbondoomsday/carbondioxide/models.py
@@ -5,5 +5,9 @@ from django.db import models
 
 class CO2Measurement(models.Model):
     """CO2 measurement model."""
+    class Meta:
+        verbose_name = "CO2 Measurement "
+        verbose_name_plural = "CO2 Measurements"
+
     date = models.DateField(unique=True)
     ppm = models.DecimalField(max_digits=5, decimal_places=2)
