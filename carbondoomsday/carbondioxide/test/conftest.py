acdd745193daefc1fcc6984af14b2f0e0b438020
Luke Murphy <lukewm@riseup.net>
2017-06-22 18:46:39 +0100

    Add mocked fixture for historical data.

--- carbondoomsday/carbondioxide/test/conftest.py
+++ carbondoomsday/carbondioxide/test/conftest.py
@@ -27,6 +27,27 @@ def mocked_latest_co2_csv(mocker):
 
 
 @pytest.fixture
+def mocked_historic_co2_csv(mocker):
+    mocked_csv = (
+        "# FOO\n"
+
+        "MLO 1974 1 1 0 0 0 -999.99 -99.99 0 19.536 "
+        "-155.576 3437.0 3397.0 40.0 NA ...\n"
+
+        "MLO 1974 9 17 0 0 0 326.832 0.235 6 19.536 "
+        "-155.576 3437.0 3397.0 40.0 NA ...\n"
+
+        "MLO 1974 12 25 0 0 0 329.775 0.136 21 19.536 "
+        "-155.576 3437.0 3397.0 40.0 NA *..\n"
+    )
+
+    mocked = mocker.Mock()
+    mocked.content = bytes(mocked_csv, "utf-8")
+    target = "carbondoomsday.carbondioxide.tasks.requests.get"
+    mocker.patch(target, return_value=mocked)
+
+
+@pytest.fixture
 def co2_measurement():
     today, ppm = date.today(), Decimal("449.15")
     return CO2Measurement.objects.create(date=today, ppm=ppm)
