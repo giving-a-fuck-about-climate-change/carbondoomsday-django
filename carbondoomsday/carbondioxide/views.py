bc9a448081a664fd5d58314da543bfa5039b6765
Luke Murphy <lukewm@riseup.net>
2017-06-23 14:11:15 +0100

    Add better docstring for the CO2 end-point.
    Closes https://github.com/giving-a-fuck-about-climate-change/carbondoomsday/issues/31.

--- carbondoomsday/carbondioxide/views.py
+++ carbondoomsday/carbondioxide/views.py
@@ -8,7 +8,24 @@ from carbondoomsday.carbondioxide.serializers import CO2MeasurementSerializer
 
 
 class CO2MeasurementViewSet(viewsets.ReadOnlyModelViewSet):
-    """CO2 measurements from the Mauna Loa observatory."""
+    """CO2 measurements from the Mauna Loa observatory.
+
+    This data is made available through the good work of the people at the
+    Mauna Loa observatory. Their release notes say:
+
+        These data are made freely available to the public and the scientific
+        community in the belief that their wide dissemination will lead to greater
+        understanding and new scientific insights.
+
+    We currently scrape the following sources:
+
+      * [co2_mlo_weekly.csv]
+      * [co2_mlo_surface-insitu_1_ccgg_DailyData.txt]
+
+    [co2_mlo_weekly.csv]: https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/co2_mlo_weekly.csv
+    [co2_mlo_surface-insitu_1_ccgg_DailyData.txt]: ftp://aftp.cmdl.noaa.gov/data/trace_gases/co2/in-situ/surface/mlo/co2_mlo_surface-insitu_1_ccgg_DailyData.txt
+
+    """ # noqa
     lookup_field = "date"
     queryset = CO2Measurement.objects.all()
     serializer_class = CO2MeasurementSerializer
