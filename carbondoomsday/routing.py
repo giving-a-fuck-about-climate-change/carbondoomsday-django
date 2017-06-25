d9422ac703b010e66f4f4b54071cf185943b4153
Luke Murphy <lukewm@riseup.net>
2017-06-25 15:49:02 +0100

    Setup first channel route for the front-end.

--- carbondoomsday/routing.py
+++ carbondoomsday/routing.py
@@ -1,6 +1,9 @@
 """Channels routing."""
 
-from channels.routing import route, route_class  # noqa
-from channels.staticfiles import StaticFilesConsumer  # noqa
+from channels.routing import route_class
 
-channel_routing = []
+from carbondoomsday.carbondioxide.consumers import FrontEndConsumer
+
+channel_routing = [
+    route_class(FrontEndConsumer,  path=r"^/frontend/"),
+]
