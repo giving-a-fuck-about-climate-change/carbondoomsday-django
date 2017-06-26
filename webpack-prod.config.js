61db20c1962f55d621e1fdee521dd35b9a86d6a4
Luke Murphy <lukewm@riseup.net>
2017-06-26 01:22:45 +0100

    Fix up the webpack production settings.

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,8 +1,11 @@
-var config        = require("./webpack.config.js");
-var BundleTracker = require("webpack-bundle-tracker");
+var config        = require("./webpack.config.js")
+var path          = require("path")
+var BundleTracker = require("webpack-bundle-tracker")
 
-config.output.path = require("path").resolve("./frontend/dist");
+config.output.path = path.resolve("./frontend/dist/")
+
 config.plugins = [
-    new BundleTracker({filename: "./webpack-stats-prod.json"})
+    new BundleTracker({filename: "./webpack-stats-prod.json"}),
 ]
-module.exports = config;
+
+module.exports = config
