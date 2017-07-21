bfb7f71bc94e77f13a42384394beb354b8f29f31
Luke Murphy <lukewm@riseup.net>
2017-07-21 20:49:25 +0200

    Use an explicit verbose name (for admin panels).

--- carbondoomsday/measurements/models.py
+++ carbondoomsday/measurements/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 
 class CO2(models.Model):
-    """CO2 model."""
+    """CO2 measurement model."""
+    class Meta:
+        verbose_name = "CO2"
+        verbose_name_plural = "CO2s"
+
     date = models.DateField(unique=True)
     ppm = models.DecimalField(max_digits=5, decimal_places=2)
 
