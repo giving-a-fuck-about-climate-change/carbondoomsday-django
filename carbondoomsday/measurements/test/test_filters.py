3416a821344b7082a08dfb4faf68dc97854e525a
Luke Murphy <lukewm@riseup.net>
2017-12-19 23:50:17 +0100

    Following 9e3ce6d, just use the basic ones.

--- carbondoomsday/measurements/test/test_filters.py
+++ carbondoomsday/measurements/test/test_filters.py
@@ -12,13 +12,7 @@ pytestmark = pytest.mark.django_db()
 def test_co2_filtering(client, co2):
     url = reverse('co2-list')
 
-    response = client.get(url, {'ppm__gte': Decimal('500.00')})
-    assert response.json()['count'] == 0
-
-    response = client.get(url, {'ppm__lte': Decimal('500.00')})
-    assert response.json()['count'] == 1
-
-    response = client.get(url, {'date__gte': date(2666, 6, 6)})
+    response = client.get(url, {'ppm': Decimal('500.00')})
     assert response.json()['count'] == 0
 
     response = client.get(url, {'date': co2.date})
