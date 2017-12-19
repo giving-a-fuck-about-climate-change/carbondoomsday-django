66e5691d927397c5953703fed4a40aaa4a7d3ce6
Luke Murphy <lukewm@riseup.net>
2017-12-19 23:30:19 +0100

    Use the new default recommended white noise setup.

--- carbondoomsday/settings.py
+++ carbondoomsday/settings.py
@@ -150,7 +150,9 @@ class Base(MLODataSources, RedisCache, GitterWebHooks,
 
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    STATICFILES_STORAGE = (
+        'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    )
 
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
